298c00cbec5ea137603e6c13d095f9c6ed56abbf
annyguzmanh96 <anamariaguzman1996@hotmail.com>
2022-05-20 00:03:31 -0500

    Codigo Mensaje_Encriptado

--- msjEncriptado.js
+++ msjEncriptado.js
@@ -26,14 +26,25 @@ const largoAlfabetoIngles=alfabetoIngles.length; //Largo del alfabeto inglés
 function encriptar(mensaje){ //Recibe como parámetro el mensaje a encriptar
    if(mensaje.length<=26){ //Condición para verificar que el largo del mensaje sea menor o igual a 26 acorde a las instrucciones
       mensaje=mensaje.split('');//pasando de strings a array el msj, con el método split, para poder jugar mejor con los datos.
-      clave={}; //se define un objeto denominado 'clave' en donde irán como key los caracteres del msj original recibido por argumento y como value, serán los carcateres aleaterios de encriptación
-      let mensajeEncriptado= mensaje.map(caracter=>{ //Creo el msj encriptado como array con el metodo.map
+      let caracteresUnicos=mensaje.filter((caract,index)=> { //Para obtener los caracteres únicos del mensaje recibido, se aplica el método filter al array del msj y como condición de filtrado para devolver el nuevo array se usa el método indexof y se compara con el indice del caracter examinado para así eliminar elmentos duplicados del array
+               return(mensaje.indexOf(caract)===index); //Uso del método indexOf
+      })
+      let clave={}; //se define un objeto denominado 'clave' en donde irán como key los caracteres del msj original recibido por argumento y como value, serán los carcateres aleaterios de encriptación
+      let objetoClaveEncriptado= caracteresUnicos.map(caracter=>{ //Creo el msj encriptado como array con el metodo.map
          let newCaracter=alfabetoIngles[Math.floor(Math.random() *(largoAlfabetoIngles))]; //el nuevo caracter que pisará el caracter del msj original se toma de la variable antes definida como 'alfabetoIngles' (un array) y se aplica el método Math.random() para tomarlo de forma aleatoria
          clave[caracter]=newCaracter; //se crea el objeto 'clave' co key=caracter (del msj original) y value=newCaracter(tomado aleatoriamente del alfabetoIngles)
-         return(newCaracter); //Retorna por cada iteración un elemento del nuevo array que se crea con el método .map, con el nuevo caracter que va reemplazando el caracter del msj original
+         return(newCaracter); //Retorna por cada iteración un elemento del nuevo array que se crea con el método .map, con los caracteres generados para a continuacion, enriptar el msj original
       })
-      mensajeEncriptado=mensajeEncriptado.join(''); //Se pasa el mensajeEncriptado de array a strings
-      return[mensajeEncriptado, clave]; //se retorna el msj encriptado y el objeto 'clave' con la info usada para encriptar
+      console.log(objetoClaveEncriptado); //Sólo por verficar
+
+      let encriptado = [];
+      for(let i=0; i<mensaje.length; i++) {
+         if(clave.hasOwnProperty(mensaje[i])) {
+            encriptado.push(clave[mensaje[i]]);
+         }
+      }
+      encriptado=encriptado.join('');
+      return [encriptado,clave];
    }else{
       return('El mensaje no puede tener más de 26 caracteres diferentes'); //Si el msj a encriptar tiene mas de 26 caracteres
    }
@@ -51,13 +62,16 @@ function invert(clavee){ //función recibe un objeto como argumento
    return newClave; //Se retorna el nuevo objeto 'newClave'
 }
 
-function decodificador(mensajeEncriptado1, claveEncriptado1){ //decodificador, recibe como argumento un msj encriptado y la clave con los valores usados para encriptar
-   console.log(mensajeEncriptado1); //Verficación por consola
-   let claveEncriptado1Inv= invert(claveEncriptado1);//Se invierte la clave para tener ahora como value, los caracteres del mensaje original
-   mensajeDecodificado=[]; //para guardar en un array los value de las key de la 'claveEncriptado1Inv' que son en realidad los caracteres de msj orginal
-   for(key in claveEncriptado1Inv){ //Recorriendo el obj 'claveEncriptado1Inv'
-      mensajeDecodificado.push(claveEncriptado1Inv[key]); //guardando en un arreglo los value con los carateres del msj original
-   }
-   mensajeDecodificado=mensajeDecodificado.join(''); //Se convierte el mensajeDecodificado en strings
-   return(mensajeDecodificado); //se visualiza el mensaje ya Decodificado
+function decodificador(encriptado, clave){ //decodificador, recibe como argumento un msj encriptado y la clave con los valores usados para encriptar
+   console.log(encriptado); //Verficación por consola
+   let claveInv= invert(clave);//Se invierte la clave para tener ahora como value, los caracteres del mensaje original
+   let desencriptado=[]; //para guardar en un array los value de las key de la 'claveInv' que son en realidad los caracteres de msj orginal
+      for(let i=0; i<encriptado.length; i++) {
+         if(claveInv.hasOwnProperty(encriptado[i])) {
+            desencriptado.push(claveInv[encriptado[i]]);
+         }
+      }
+      desencriptado=desencriptado.join('');
+   
+   return desencriptado
 }	
